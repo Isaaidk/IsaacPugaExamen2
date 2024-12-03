fcac5032609918ce52a1649201768fad684e2430
Isaaidk <isaacpuga661@gmail.com>
2024-12-03 08:54:12 -0500

    Creacion y configuracion de la clase para ver las notas

--- IsaacPugaExamen2/IPtelefonossave.xaml.cs
+++ IsaacPugaExamen2/IPtelefonossave.xaml.cs
@@ -4,6 +4,6 @@ public partial class IPtelefonossave : ContentPage
 {
 	public IPtelefonossave()
 	{
-		InitializeComponent();
-	}
+        BindingContext = new Models.AllNotes();
+    }
 }
---
+++ IsaacPugaExamen2/Resources/Models/Telefonos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsaacPugaExamen2.Resources.Models
+{
+    internal class Telefonos
+    {
+        public ObservableCollection<IPTelefono> Notes { get; set; } = new ObservableCollection<IPTelefono>();
+
+        public Telefonos() =>
+            LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+
+            // Get the folder where the notes are stored.
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            // Use Linq extensions to load the *.notes.txt files.
+            IEnumerable<IPTelefono> notes = Directory
+
+                                        // Select the file names from the directory
+                                        .EnumerateFiles(appDataPath, "*.notes.txt")
+
+                                        // Each file name is used to create a new Note
+                                        .Select(filename => new IPTelefono()
+                                        {
+                                            Filename = filename,
+                                            Telefono = File.ReadAllText(filename),
+                                            Nombre = File.ReadAllText(filename)
+                                        })
+
+                                        // With the final collection of notes, order them by date
+                                        .OrderBy(note => note.Nombre);
+
+            // Add each note into the ObservableCollection
+            foreach (IPTelefono note in notes)
+                Notes.Add(note);
+        }
+    }
+}
+}
