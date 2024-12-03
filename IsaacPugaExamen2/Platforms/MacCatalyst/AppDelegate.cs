042e9abd21dfd3e98a357b00553f845a358afd6b
Isaaidk <isaacpuga661@gmail.com>
2024-12-03 07:19:07 -0500

    Agregar archivos de proyecto.

---
+++ IsaacPugaExamen2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IsaacPugaExamen2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
