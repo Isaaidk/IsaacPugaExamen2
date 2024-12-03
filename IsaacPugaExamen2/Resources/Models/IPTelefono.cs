ec3d394454518b22a102d2d85bf5107a3454a19a
Isaaidk <isaacpuga661@gmail.com>
2024-12-03 07:30:32 -0500

    Creacion de carpetas de separacion de vistas y modelos , con la creacion de el modelo-clase IPTelefono

---
+++ IsaacPugaExamen2/Resources/Models/IPTelefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsaacPugaExamen2.Resources.Models
+{
+    public class IPTelefono
+    {
+        public String Nombre { get; set; }
+        public int Telefono  { get; set; }
+
+    }
+}
